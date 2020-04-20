fe80d7d983acffe965c6449b23c06f450b1fde11
Logan Lindquist <logan@llbbl.com>
2020-04-20 15:58:43 -0500

    github button and fixing text decoration

--- src/templates/post.js
+++ src/templates/post.js
@@ -7,7 +7,7 @@ export default function Template({
                                      data, // this prop will be injected by the GraphQL query below.
                                  }) {
     const { markdownRemark } = data // data.markdownRemark holds your post data
-    const { frontmatter, html } = markdownRemark
+    const { frontmatter } = markdownRemark
 
     return <Layout>
         <Helmet>
