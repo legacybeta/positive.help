419e52768d7c7bf8e109258391fdda36cba4d831
llbbl <logan@llbbl.com>
2025-02-02 13:57:02 -0600

    Add user and account tables for authentication support

--- db/schema.ts
+++ db/schema.ts
@@ -9,3 +9,26 @@ export const messages = sqliteTable("messages", {
 	date: numeric(),
 });
 
+
+
+export const users = sqliteTable('users', {
+  id: text('id').primaryKey(),
+  name: text('name'),
+  email: text('email').unique(),
+  emailVerified: integer('emailVerified', { mode: 'timestamp_ms' }),
+  image: text('image'),
+});
+
+export const accounts = sqliteTable('accounts', {
+  userId: text('userId').notNull(),
+  type: text('type').notNull(),
+  provider: text('provider').notNull(),
+  providerAccountId: text('providerAccountId').notNull(),
+  refresh_token: text('refresh_token'),
+  access_token: text('access_token'),
+  expires_at: integer('expires_at'),
+  token_type: text('token_type'),
+  scope: text('scope'),
+  id_token: text('id_token'),
+  session_state: text('session_state'),
+});
